b25167406a41cb814ebdf4b7dc051c0c5ebbe024
Bayzeedsakib <angelsakib4@gmail.com>
2026-03-19 05:39:37 +0600

    update AuthorController:  add details and Edit option

--- Author-Book Directory/Controllers/AuthorController.cs
+++ Author-Book Directory/Controllers/AuthorController.cs
@@ -35,5 +35,30 @@ namespace Author_Book_Directory.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Details(int id)
+        {
+            var data = (from a in db.Authors where a.Id == id select a).SingleOrDefault();
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var data = db.Authors.Find(id);
+            string beforeName = data.Name;
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Author a)
+        {
+            var data = db.Authors.Find(a.Id);
+            string beforeName = data.Name;
+            data.Name = a.Name;
+            db.SaveChanges();
+
+            TempData["Msg"] = "from " + beforeName + " to " + a.Name + " edit successfully";
+            return RedirectToAction("Index");
+        }
     }
 }
